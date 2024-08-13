c3424f8dcdb36210ad73f56b434f9212885095b2
Victor Oldensand <victormag24@gmail.com>
2024-08-13 21:23:02 +0200

    Initial Commit

---
+++ Utter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Utter
+//
+//  Created by Victor Magnus Oldensand on 2024-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Utter/UtterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtterApp.swift
+//  Utter
+//
+//  Created by Victor Magnus Oldensand on 2024-08-13.
+//
+
+import SwiftUI
+
+@main
+struct UtterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
