b5c999aa8635d588a599446401106e23c588e31c
Victor Oldensand <victormag24@gmail.com>
2024-08-14 11:58:28 +0200

    Refactor: move code in my file system

---
+++ Utter/Configuration.swift
@@ -0,0 +1,30 @@
+//
+//  Configuration.swift
+//  Utter
+//
+//  Created by Victor Magnus Oldensand on 2024-08-14.
+//
+
+import Foundation
+
+enum Configuration {
+    enum Error: Swift.Error {
+        case missingKey, invalidValue
+    }
+
+    static func value<T>(for key: String) throws -> T where T: LosslessStringConvertible {
+        guard let object = Bundle.main.object(forInfoDictionaryKey:key) else {
+            throw Error.missingKey
+        }
+
+        switch object {
+        case let value as T:
+            return value
+        case let string as String:
+            guard let value = T(string) else { fallthrough }
+            return value
+        default:
+            throw Error.invalidValue
+        }
+    }
+}
