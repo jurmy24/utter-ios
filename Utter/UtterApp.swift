b5a6cba4715c595bee1be3c70fc20b52d96ae7de
Victor Oldensand <victormag24@gmail.com>
2024-09-03 21:12:05 +0200

    Build: add firebase configuration

--- Utter/UtterApp.swift
+++ Utter/UtterApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("Configured Firebase!")
+        return true
+    }
+}
 
 @main
 struct UtterApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
