d503dba2caecff173df4e0112f1d1eed818e1d03
Victor Oldensand <victormag24@gmail.com>
2024-09-17 22:54:24 +0200

    Feat: add storage manager and connect to story view to fetch json files from the firebase storage

--- Utter/Core/Story/StoryView.swift
+++ Utter/Core/Story/StoryView.swift
@@ -9,27 +9,57 @@ import SwiftUI
 
 final class StoryViewModel: ObservableObject {
     //    @Published var blocks:
+//    @Published var jsonData = nil
+    @Published var storyString: String = ""
+    
+    func loadStory(path: String) async throws {
+        let data = try await StorageManager.shared.getStory(path: path)
+        
+        self.storyString = String(data: data, encoding: .utf8) ?? "It didn't work"
+        
+    }
+    
 }
 
 struct StoryView: View {
     
+    let story: StoryWithProgress
     @StateObject private var viewModel = StoryViewModel()
+    @State private var textInfo: String = ""
     
     var body: some View {
         ScrollView {
-            VStack (alignment: .leading, spacing: 20){
-                let blocks = [1, 2, 3]
-                ForEach(blocks, id: \.self) { block in
-                    TextBlob(avatar: "person.circle.fill", text: "Tjenare mannen, det går bra för min del. Hur går det för dig? Hur går det för dig? Hur går det för dig?", modifier: .hideAudio)
-                        .padding(.horizontal, 16)
-                }
-                
-                Spacer()
+//            VStack (alignment: .leading, spacing: 30){
+//                let blocks = [1, 2, 3]
+//                ForEach(blocks, id: \.self) { block in
+//                    TextBlob(avatar: "person.circle.fill", text: "Tjenare mannen, det går bra för min del. Hur går det för dig? Hur går det för dig? Hur går det för dig?", modifier: .hideAudio)
+//                        .padding(.horizontal, 16)
+//                }
+//                
+//                Spacer()
+//            }
+            // TODO: print the json file in the Text() here
+            Text(textInfo)
+            
+//            if let jsonData, let jsonData = JSONSerialization(jsonData) {
+//                Text
+//            }
+        }
+        .task {
+            guard let storageLocation = story.story.storageLocation else {
+                textInfo = "storageLocation not found"
+                return
+            }
+            do {
+                try await viewModel.loadStory(path: storageLocation)
+                textInfo = viewModel.storyString
+            } catch {
+                textInfo = "unable to load story"
             }
         }
     }
 }
 
-#Preview {
-    StoryView()
-}
+//#Preview {
+//    StoryView()
+//}
--- Utter/Storage/StorageManager.swift
+++ Utter/Storage/StorageManager.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import FirebaseStorage
+import UIKit
+
+final class StorageManager {
+    
+    static let shared = StorageManager()
+    private init() {}
+    
+    private let storage = Storage.storage().reference()
+    private let gsStorage = Storage.storage()
+    
+    private var imagesReference: StorageReference {
+        storage.child("images")
+    }
+    
+    private func userReference(userId: String) -> StorageReference {
+        storage.child("users").child(userId)
+    }
+    
+    func saveImage(data: Data, userId: String) async throws -> (path: String, name: String) {
+        
+        let meta = StorageMetadata()
+        
+        meta.contentType = "image/jpeg"
+        
+        let path = "\(UUID().uuidString).jpeg"
+        let returnedMetaData = try await userReference(userId: userId).child(path).putDataAsync(data, metadata: meta)
+        
+        guard let returnedPath = returnedMetaData.path, let returnedName = returnedMetaData.name else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return (returnedPath, returnedName)
+    }
+    
+    func saveImage(image: UIImage, userId: String) async throws -> (path: String, name: String) {
+        // image.pngData()
+        guard let data = image.jpegData(compressionQuality: 1) else {
+            throw URLError(.backgroundSessionWasDisconnected)
+        }
+        
+        return try await saveImage(data: data, userId: userId)
+    }
+    
+    func getData(userId: String, path: String) async throws -> Data {
+        try await userReference(userId: userId).child(path).data(maxSize: 3 * 1024 * 1024)
+    }
+    
+    func getStory(path: String) async throws -> Data {
+        try await gsStorage.reference(forURL: path).data(maxSize: 1 * 1024 * 1024)
+    }
+    
+}
